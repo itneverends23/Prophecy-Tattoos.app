ecec25e37afd2773da4346629b50e295562cdf18
itneverends23 <118855139+itneverends23@users.noreply.github.com>
2022-11-23 18:48:30 -0500

    update

--- serviceworker.js
+++ serviceworker.js
@@ -1,3 +1,8 @@
-if('service.worker' in navigator){
-console.log('service worker supported');
+if('serviceWorker' in navigator){
+window.addEventListener('load',() => {
+    navigator.serviceWorker
+    .register('serviceworker.js')
+    .then(reg => console.log('service worker registered'))
+    .catch(err => console.log(`service worker : error: ${err}`))
+} )
 }
