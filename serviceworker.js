292781cb57c9b6b8dbda9679e36f95df86cf919e
itneverends23 <118855139+itneverends23@users.noreply.github.com>
2022-11-24 15:30:07 -0500

    update

--- serviceworker.js
+++ serviceworker.js
@@ -16,24 +16,27 @@ self.addEventListener('install', event => {
     console.log("[SW] installed");
     event.waitUntil(preCache())
 })
+
 self.addEventListener('activate', event => {
     console.log("[SW] activated");
+})
 
 
 async function fetchAssets(event){
-        try {
-            const response = fetch(event.request)
-            return response
-        } catch (err){
-            const cache = await caches.open(CACHE_NAME)
-            return cache.match(event.request)
-        }
-
+    try {
+        const response = await fetch(event.request)
+        return response
+    } catch (err){
+        const cache = await caches.open(CACHE_NAME)
+        return cache.match(event.request)
+    }
+    
 }
-})
+
 self.addEventListener('fetch', event => {
-    console.log("[SW] fetched");
-    event.respondWith(fetchAssets(event))
+console.log("[SW] fetched");
+event.respondWith(fetchAssets(event))
+
 })
 
 
