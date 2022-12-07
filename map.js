137868a6dea00d07757ef2cb240a0a027af42b7d
itneverends23 <118855139+itneverends23@users.noreply.github.com>
2022-12-07 18:14:15 -0500

    update map

---
+++ map.js
@@ -0,0 +1,8 @@
+function initMap(){
+
+    var options = {
+        center: {lat: 40.7128 , lng: -74.0060};
+        zoom: 6
+    }
+    map = new map google.maps.Map(document.getElementById("map"),( "options"),)
+}
