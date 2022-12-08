fe1640bea10cd04ec8836f067cbf90af4d6ead35
itneverends23 <118855139+itneverends23@users.noreply.github.com>
2022-12-07 19:37:58 -0500

    update

--- map.js
+++ map.js
@@ -4,11 +4,9 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -34.397, lng: 150.644 },
     zoom: 8,
+    draggable : false,
     
   });
 }
 
-window.initMap = initMap;
-
-map = new google.maps.Map({ draggable : false });
 
